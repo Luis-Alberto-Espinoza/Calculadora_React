400370415050d5104bce69b1c3e2499904f2a412
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2024-03-19 21:07:00 -0300

    proyecto completado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
 import './App.css';
+import logoLuis from './imagenes/2.png';
+import Boton from './componentes/Boton.js';
+import Pantalla from './componentes/Pantalla.js';
+import BtnClear from './componentes/BtnClear.js';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    if (val === "=") {
+      setInput('');
+      return;
+    }
+    if (val === "x") {
+      val = "*";
+    }
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } 
+  };
+
   return (
     <div className="App">
-      
+      <div className='contenedor-logo-principal'>
+        <img src={logoLuis}
+          className='logo'
+          alt='log de la calculadora' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input} />
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>1</Boton>
+          <Boton manejarClick={agregarInput}>2</Boton>
+          <Boton manejarClick={agregarInput}>3</Boton>
+          <Boton manejarClick={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>4</Boton>
+          <Boton manejarClick={agregarInput}>5</Boton>
+          <Boton manejarClick={agregarInput}>6</Boton>
+          <Boton manejarClick={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>7</Boton>
+          <Boton manejarClick={agregarInput}>8</Boton>
+          <Boton manejarClick={agregarInput}>9</Boton>
+          <Boton manejarClick={agregarInput}>x</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={calcularResultado}>=</Boton>
+          <Boton manejarClick={agregarInput}>0</Boton>
+          <Boton manejarClick={agregarInput}>.</Boton>
+          <Boton manejarClick={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BtnClear manejarClear={() => setInput('')}>
+            Limpiar
+          </BtnClear>
+        </div>
+      </div>
     </div>
   );
 }
